a595da672e97449c89b253b5e12e8f0de437a537
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2022-06-30 22:57:52 +1200

    been a long time but added a new landing area and made responsive.

---
+++ index.js
@@ -0,0 +1,8 @@
+// typed animation section
+var typed = new Typed('.typed', {
+  strings: ['Student', 'Web Designer', 'Web Developer', 'Coordinator'],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+  startDelay: 100,
+});
